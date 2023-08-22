3a280c78c5733f19cfe4ecbedbfb08603ab6953e
MonjedNidal <monjed1010@gmail.com>
2023-08-22 10:32:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div className="h-screen flex flex-wrap justify-center content-center ">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Drawers from "../images/drawers.jpg";
+function Main() {
+  return (
+    <div className="flex w-fit h-fit shadow-md">
+      <img src={Drawers} alt="" />
+    </div>
+  );
+}
+
+export default Main;
